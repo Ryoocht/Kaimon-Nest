fe7d5249a0894e33ba9832c669c9ea6a89991499
Ryochi <ryo1.t.zubu@gmail.com>
2022-10-20 07:48:00 +1000

    finished configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './module/auth/auth.module';
 import { StoreModule } from './module/store/store.module';
+import { PrismaModule } from './module/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, StoreModule],
+  imports: [AuthModule, StoreModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
