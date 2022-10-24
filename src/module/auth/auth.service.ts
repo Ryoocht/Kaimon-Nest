e143abfac0b4447c2c0c24eacf948fb6e35014a9
Ryochi <ryo1.t.zubu@gmail.com>
2022-10-24 21:33:52 +1000

    added prisma error

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as argon from 'argon2';
 import { LoginDto, SignupDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -12,34 +13,43 @@ export class AuthService {
   }
 
   async signup(dto: SignupDto) {
-    // Generate the Password Hash
-    const hash = await argon.hash(dto.password);
-    // Save the New User in the DB
-    const user = await this.prisma.user.create({
-      data: {
-        firstName: dto.fistName,
-        lastName: dto.lastName,
-        email: dto.email,
-        password: hash,
-        phoneNumber: dto.phoneNumber,
-        avatar: dto.avatar,
-        role: dto.role,
-      },
-      // Select Entity to Return
-      select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true,
-        phoneNumber: true,
-        createdAt: true,
-        password: true,
-      },
-    });
-    // Delete Specific Entities
-    delete user.password;
+    try {
+      // Generate the Password Hash
+      const hash = await argon.hash(dto.password);
+      // Save the New User in the DB
+      const user = await this.prisma.user.create({
+        data: {
+          firstName: dto.fistName,
+          lastName: dto.lastName,
+          email: dto.email,
+          password: hash,
+          phoneNumber: dto.phoneNumber,
+          avatar: dto.avatar,
+          role: dto.role,
+        },
+        // Select Entity to Return
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          phoneNumber: true,
+          createdAt: true,
+          password: true,
+        },
+      });
+      // Delete Specific Entities
+      delete user.password;
 
-    // Return User Object
-    return user;
+      // Return User Object
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+      throw error;
+    }
   }
 }
